96a77bc33583f64367d92a72c2924cf15353f325
Hassaan Ali <hassaan1@ualberta.ca>
2016-03-10 21:56:24 -0700

    changed producer to send messages in JSON format using gson

--- Streaming/kafka-producer/src/main/java/com/mapr/examples/Producer.java
+++ Streaming/kafka-producer/src/main/java/com/mapr/examples/Producer.java
@@ -7,14 +7,18 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 /**
  * This producer will send a bunch of messages to topic "fast-messages". Every so often,
  * it will send a message to "slow-messages". This shows how messages can be sent to
  * multiple topics. On the receiving end, we will see both kinds of messages but will
  * also see how the two topics aren't really synchronized.
  */
+
+
 public class Producer {
+
     public static void main(String[] args) throws IOException {
         // set up the producer
         KafkaProducer<String, String> producer;
@@ -23,29 +27,23 @@ public class Producer {
             properties.load(props);
             producer = new KafkaProducer<>(properties);
         }
-
         try {
-            for (int i = 0; i < 1000000; i++) {
-                // send lots of messages
-
-                // Commented out fast messages
+            GsonBuilder builder = new GsonBuilder();
+            Gson gson = builder.create();
+            Segment S = new Segment();
+            for (int i = 0; i < 100; i++) {
                 /*producer.send(new ProducerRecord<String, String>(
                         "fast-messages",
-                        String.format("{\"type\":\"test\", \"t\":%.3f, \"k\":%d}", System.nanoTime() * 1e-9, i))); */
-
-                // every so often send to a different topic
-
-                if (i % 1000 == 0) {
-                    // Commented out fast messages
-                    /*producer.send(new ProducerRecord<String, String>(
-                            "fast-messages",
-                            String.format("{\"type\":\"marker\", \"t\":%.3f, \"k\":%d}", System.nanoTime() * 1e-9, i))); */
-                    producer.send(new ProducerRecord<String, String>(
-                            "summary-markers",
-                            String.format("{\"type\":\"other\", \"t\":%.3f, \"k\":%d}", System.nanoTime() * 1e-9, i)));
-                    producer.flush();
-                    System.out.println("Sent msg number " + i);
-                }
+                        String.format("{\"type\":\"marker\", \"t\":%.3f, \"k\":%d}", System.nanoTime() * 1e-9, i))); */
+                S.startlat = Math.random() * 90 + 1;
+                S.startlong = Math.random() * 90 + 1;
+                S.cost = Math.random() * 90 + 1;
+                S.endlat = Math.random() * 90 + 1;
+                S.endlong = Math.random() * 90 + 1;
+                //producer.send(new ProducerRecord<String, String>("summary-markers", String.format("{\"type\":\"other\", \"t\":%.3f, \"k\":%d}", System.nanoTime() * 1e-9, i)));
+                producer.send(new ProducerRecord<String, String>("summary-markers", gson.toJson(S)));
+                producer.flush();
+                System.out.println("Sent msg number " + i);
             }
         } catch (Throwable throwable) {
             System.out.printf("%s", throwable.getStackTrace());
---
+++ Streaming/kafka-producer/src/main/java/com/mapr/examples/Segment.java
@@ -0,0 +1,9 @@
+package com.mapr.examples;
+
+public class Segment {
+    public double startlat;
+    public double startlong;
+    public double cost;
+    public double endlat;
+    public double endlong;
+}
