ca49a6df8b68a133cfcf8506b56c9056e94a9d70
Hassaan Ali <hassaan1@ualberta.ca>
2016-04-02 18:43:51 -0600

    changed front end to use postgres, imported osm data and made the front end query the db for osm nodes and cost.

--- DRP/app/controllers/maps_controller.rb
+++ DRP/app/controllers/maps_controller.rb
@@ -1,9 +1,59 @@
 class MapsController < ApplicationController
+
   def index
-    @segment = Segment.all
-    @hash = Gmaps4rails.build_markers(@segment) do |segment, marker|
-      marker.lat segment.startlat
-      marker.lng segment.startlong
-    end
   end
+
+
+  def calcroute
+    #empty the global nodes array every time a new request is sent.
+    nodes = []
+    # Get paths from ajax call
+    paths = JSON.parse(params[:paths])
+    i = 0
+    # For each set of nodes, find corresponding nodes in the db, create a list for each path.
+    while i < paths.length
+      nodearray = []
+      unless paths[i] == nil
+        j = 0
+        while j < paths[i].length
+          # Get a geohash for each node
+          geohash = GeoHash.encode(paths[i][j]['lat'], paths[i][j]['lng'])
+          # Get the closest matching node
+          k = 3
+          trimmedhash = geohash[0,geohash.length-k]
+          while Node.where("geohash LIKE (?)", "#{trimmedhash}%").empty?
+            k+=1
+            trimmedhash = geohash[0,geohash.length-k]
+          end
+          #add the node to the array of nodes for this path
+          nodearray.append(osmNode = Node.where("geohash LIKE (?)", "#{trimmedhash}%").first)
+          j+=1
+        end
+        # add each array of nodes to the 2d array
+        nodes.append(nodearray)
+      end
+      i+=1
+    end
+
+    # Need to find the ways associated with each node and sum the cost up.
+    routeCost = []
+    for i in 0..2
+      routeCost[i] = 0
+      unless nodes[i].nil?
+        nodes[i].each do |node|
+          way = Way.where("nodes @> ARRAY[?]::bigint[]", node.id)
+          unless way.empty?
+            unless way.first.cost.nil?
+              routeCost[i] += way.first.cost
+            end
+          end
+        end
+      end
+    end
+    #The index of the best route
+    #gon.bestroute = routeCost.index(routeCost.min)
+    best = routeCost.index(routeCost.min)
+    render :json => {:routeindex => best}
+  end
+
 end
--- DRP/config/routes.rb
+++ DRP/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get 'maps/index'
   root 'maps#index'
+  post '/calcroute'=> 'maps#calcroute'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
